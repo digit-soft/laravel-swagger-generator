9b146c371916d319f6f055ed5331ac582e45a9d1
digit <digit.vova@gmail.com>
2018-12-18 11:57:37 +0200

    'laravel-swagger-generator' - Add Secured annotation

---
+++ oa/Secured.php
@@ -0,0 +1,60 @@
+<?php
+namespace OA;
+
+/**
+ * @Annotation
+ * @Target({"METHOD"})
+ * @Attributes({
+ *   @Attribute("scheme", type = "string"),
+ * })
+ */
+class Secured extends BaseAnnotation
+{
+    public $scheme;
+
+    public $content = [];
+
+    /**
+     * Secured constructor.
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        $this->configureSelf($values, 'scheme');
+    }
+
+    /**
+     * Get object string representation
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->scheme;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function toArray()
+    {
+        return $this->getSecurityScheme();
+    }
+
+    /**
+     * Get security scheme data
+     * @return array
+     * @throws \Exception
+     */
+    protected function getSecurityScheme()
+    {
+        $schemes = config('swagger-generator.content.components.securitySchemes', []);
+        if (empty($schemes)) {
+            throw new \Exception("There are no security schemes defined");
+        }
+        $key = $this->scheme ?? key($schemes);
+        if (!isset($schemes[$key])) {
+            throw new \Exception("Security scheme '{$key}' not defined");
+        }
+        return [$key => $this->content];
+    }
+}
