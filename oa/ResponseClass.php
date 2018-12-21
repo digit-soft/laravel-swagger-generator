a445efa66b268597ccd0f9c79ad97ed0e12dfea4
digit <digit.vova@gmail.com>
2018-12-21 09:36:07 +0200

    'laravel-swagger-generator' - Simplify ResponseClass annotation

--- oa/ResponseClass.php
+++ oa/ResponseClass.php
@@ -2,10 +2,9 @@
 
 namespace OA;
 
-use DigitSoft\Swagger\DumperYaml;
 use DigitSoft\Swagger\Parser\WithAnnotationReader;
 use DigitSoft\Swagger\Parser\WithReflections;
-use DigitSoft\Swagger\Parsers\ClassParser;
+use DigitSoft\Swagger\Yaml\Variable;
 use Doctrine\Common\Annotations\Annotation\Target;
 
 /**
@@ -29,10 +28,7 @@ class ResponseClass extends Response
         if ($this->content === null || !class_exists($this->content)) {
             throw new \Exception("Class '{$this->content}' not found");
         }
-        return [
-            'type' => 'object',
-            'properties' => $this->getModelProperties(),
-        ];
+        return $this->getModelProperties();
     }
 
     /**
@@ -55,35 +51,12 @@ class ResponseClass extends Response
      */
     protected function getModelProperties()
     {
-        $parser = new ClassParser($this->content);
-        $properties = $parser->properties(true);
-        $propertiesRead = [];
-        if (($with = $this->getWith()) !== null) {
-            $propertiesRead = $parser->propertiesRead($with);
-        }
-        $properties = array_merge($properties, $propertiesRead);
-        $result = [];
-        foreach ($properties as $varName => $varData) {
-            $result[$varName] = $this->describeProperty($varName, $varData);
-        }
-        $propertiesAnn = $this->getModelPropertyAnnotations();
-        $result = DumperYaml::merge($result, $propertiesAnn);
-        return $result;
-    }
-
-    /**
-     * Get properties by annotations
-     * @return array
-     */
-    protected function getModelPropertyAnnotations()
-    {
-        /** @var \OA\Property[] $annotations */
-        $annotations = $this->classAnnotations($this->content, 'OA\Property');
-        $result = [];
-        foreach ($annotations as $annotation) {
-            $result[$annotation->name] = $annotation->toArray();
-        }
-        return $result;
+        $variable = Variable::fromDescription([
+            'type' => $this->content,
+            'with' => $this->getWith(),
+            'description' => $this->description,
+        ]);
+        return $variable->describe();
     }
 
     /**
@@ -96,29 +69,5 @@ class ResponseClass extends Response
             return $this->with = [$this->with];
         }
         return $this->with;
-    }
-
-    /**
-     * Describe model property
-     * @param string $propName
-     * @param array $propData
-     * @return array
-     */
-    private function describeProperty($propName, $propData = [])
-    {
-        if (isset($propData['properties'])) {
-            foreach ($propData['properties'] as $propNameNested => $propDataNested) {
-                $propData['properties'][$propNameNested] = $this->describeProperty($propNameNested, $propDataNested);
-            }
-        } elseif($propData['type'] === 'array' && isset($propData['items'])) {
-            return $propData;
-        } else {
-            if (strpos($propData['type'], '|') !== false) {
-                $propData['type'] = explode('|', $propData['type'])[0];
-            }
-            $propValue = DumperYaml::getExampleValue($propData['type'], $propName);
-            $propData = array_merge($propData, DumperYaml::describe($propValue));
-        }
-        return $propData;
     }
 }
