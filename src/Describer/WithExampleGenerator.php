3f417021de2fbca487510ae8b3f27f669e106e97
digit <digit.vova@gmail.com>
2019-03-13 11:17:57 +0200

    Update WithExampleGenerator trait to generate more stable examples (for GIT file versions)

--- src/Describer/WithExampleGenerator.php
+++ src/Describer/WithExampleGenerator.php
@@ -3,6 +3,8 @@
 namespace DigitSoft\Swagger\Describer;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Str;
 
 /**
  * Trait WithExampleGenerator
@@ -16,6 +18,10 @@ trait WithExampleGenerator
      * @var array Generated variables cache
      */
     protected $varsCache = [];
+    /**
+     * @var array Generated sequences
+     */
+    protected $varsSequences = [];
 
     /**
      * Get variable example
@@ -56,6 +62,264 @@ trait WithExampleGenerator
     }
 
     /**
+     * Generate example sequence by type
+     * @param  string $type
+     * @param  int    $count
+     * @return array
+     */
+    protected function generateExampleByTypeSequence($type, $count = 10)
+    {
+        $type = is_string($type) ? $this->normalizeType($type, true) : null;
+        $sequence = [];
+        for ($i=1; $i <= $count; $i++) {
+            $elem = $this->exampleByTypeSequential($type, $i);
+            $sequence[] = $elem;
+            if ($elem === null) {
+                break;
+            }
+        }
+        return $sequence;
+    }
+
+    /**
+     * Generate example sequence by rule
+     * @param  string $rule
+     * @param  int    $count
+     * @return array
+     */
+    protected function generateExampleByRuleSequence($rule, $count = 10)
+    {
+        $sequence = [];
+        for ($i=1; $i <= $count; $i++) {
+            $elem = $this->exampleByRuleSequential($rule, $i);
+            $sequence[] = $elem;
+            if ($elem === null) {
+                break;
+            }
+        }
+        return $sequence;
+    }
+
+    /**
+     * Get example by given type for sequence
+     * @param  string    $type
+     * @param  int $iteration
+     * @return mixed
+     */
+    protected function exampleByTypeSequential($type, $iteration = 1)
+    {
+        $dateStr = '2000-01-01 00:00:00';
+        switch ($type) {
+            case 'int':
+            case 'integer':
+                return intval($iteration * 3);
+                break;
+            case 'float':
+            case 'double':
+                return 1.65 * $iteration;
+                break;
+            case 'string':
+                $strArr = ['string', 'str value', 'str example', 'string data', 'some txt'];
+                return $this->takeFromArray($strArr, $iteration);
+                break;
+            case 'bool':
+            case 'boolean':
+                return $iteration % 2 ? true : false;
+                break;
+            case 'date':
+                $date = Date::createFromFormat('Y-m-d H:i:s', $dateStr);
+                $date->addSeconds($iteration * 800636);
+                return $date->format('Y-m-d');
+                break;
+            case 'Illuminate\Support\Carbon':
+            case 'dateTime':
+            case 'datetime':
+                $date = Date::createFromFormat('Y-m-d H:i:s', $dateStr);
+                $date->addSeconds($iteration * 800636);
+                return $date->format('Y-m-d H:i:s');
+                break;
+            case 'array':
+                return [];
+                break;
+        }
+        return null;
+    }
+
+    /**
+     * Get example value by validation rule
+     * @param  string $rule
+     * @param  int    $iteration
+     * @return mixed
+     */
+    protected function exampleByRuleSequential(string $rule, $iteration = 1)
+    {
+        $dateStr = '2019-01-01 00:00:00';
+        switch ($rule) {
+            case 'phone':
+                $strArr = ['+380971234567', '+380441234567', '+15411234567', '+4901511234567'];
+                $example = $this->takeFromArray($strArr, $iteration);
+                break;
+            case 'url':
+                $example = 'http://example.com/url-' . $iteration . '-generated';
+                break;
+            case 'image':
+                $example = 'https://lorempixel.com/640/480/?' . $iteration * 6842;
+                break;
+            case 'email':
+                $examples = [
+                    "nikolaus.jo@haag.net", "oral46@gleichner.com", "triston73@gmail.com", "sedrick.russel@gmail.com",
+                    "christian64@hotmail.com", "lwill@baumbach.com", "stanton.nicolas@schulist.net", "fisher.benedict@yahoo.com",
+                    "jaren85@dicki.info", "thiel.maxwell@ortiz.org",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'password':
+                $examples = [
+                    "FwsU63aIflde0t2x", "nYZxNonFfkPwmKRB", "cLRdk4y0yVdK3QP4", "LkWwjjdaVK1pGDQf", "LdKBH0RXvlOOD6kg",
+                    "YOgNltJWQrWf5AmQ", "8E4BbRtJO4MgRlJP", "KLuOcU5EYjhLqbHB", "zr3rZ5GNu1oSaeHQ", "VpCgk7BS2QP2X5VT",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'token':
+                $examples = [
+                    "AmUfolr4CMVihtjvHgPcA3IAPGmV9Vknr44sAdAYcmNauKXBssVOjQrZFPlizrKO",
+                    "KDr5Js6MkvH6XSDdgLF0sQv8RQvDBla3I2YADdtSON3JFK10sYIARZvL7MHv7FG8",
+                    "aIoZAUw6cfcdOpKhlTFl6btgdbWAazQeujD35aFg6mW2c3RKYgtqtF7i03Vfe4Du",
+                    "LeJuw7tMULngkbeePqA61Nv88Y4tZWRJkrW7ISkGMRdhYUuh9MhFJVdye81hlQQ1",
+                    "C6ZJFs4zgJ3ejE13RVJvAwDG1fgv4w4nMoo5MbMDjp4y3JkFibWnrTlmutIQdBtH",
+                    "tBWDClhFDE0kZJlL0v2iVp1xDJGCakrc3S7M4Btcc19f7x5SqbzxLRtvPMmdKcgO",
+                    "ZOyU88s36oiCheUTxWwnDW0D21WkzG5RVl7x47Mo7hFmwJkHI4g9LoVgug6jSdsC",
+                    "AGoIz32MiUcX7W58zeCThiQgyeMoiDa9As4cJz5lJPNaJyOh3XLKovKS69HJ4y6s",
+                    "Z63TlMogLQ7ibz92psjTO8KrkVgp9KYuOFldOXcvbv2icpPtaaW08ekGqj0b7O8s",
+                    "ITbeGihCUzEnolxEZbjSWYYTHxQDrNQIaHFPIdJfT36yZ1KimVxKN9b240NEsNpw",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'service_name':
+                $example = $this->takeFromArray(['fb', 'google', 'twitter'], $iteration);
+                break;
+            case 'domain_name':
+                $example = 'example-' . $iteration . '.com';
+                break;
+            case 'alpha':
+            case 'string':
+                $example = $this->takeFromArray(['string', 'value', 'str value'], $iteration);
+                break;
+            case 'text':
+                $examples = [
+                    "Ut tempora hic iusto assumenda. In aut quae possimus provident.",
+                    "Culpa eius voluptate quae accusantium aut aut. Et ipsa quia aut sint facilis pariatur.",
+                    "Et itaque qui omnis vero aut. Ipsa esse quae error sed enim. Est et ad similique.",
+                    "Id aut et voluptatum odio rerum sint veritatis. Omnis dolores quisquam animi.",
+                    "Omnis et sed sapiente ab. Consequatur voluptatem occaecati nihil atque et.",
+                    "Beatae optio aperiam voluptatem dolor facere. Nesciunt cum ullam accusantium enim.",
+                    "Illo ut eum sint. Possimus est quo vel assumenda. Sequi dolorem minus atque et iusto.",
+                    "Dolores tempora quasi fugit alias. Soluta expedita autem dolor quasi minus.",
+                    "Sequi nulla omnis quis atque. Sint et adipisci magni qui. Laboriosam et saepe tempora vel.",
+                    "Delectus quia illo quia et molestiae. Vitae ex non modi sed iste non velit.",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'textShort':
+                $examples = [
+                    "Soluta quisquam qui tenetur molestias sequi.",
+                    "Quia et rerum tenetur.",
+                    "Ea est labore est sit et id.",
+                    "Aut qui sed ut reprehenderit beatae est qui.",
+                    "Ducimus id sed eaque id doloremque.",
+                    "Odit molestias porro quia natus est quo.",
+                    "Cupiditate aut aut sunt dolor ab sunt sunt.",
+                    "Velit ut odio dolorem deleniti quas.",
+                    "Consectetur at molestias repellendus.",
+                    "Velit fuga culpa et et consequatur ea maxime.",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'alpha_num':
+                $example = $this->takeFromArray(['string35', 'value90', 'str20value'], $iteration);
+                break;
+            case 'alpha_dash':
+                $example = $this->takeFromArray(['string_35', 'value-90', 'str_20-value'], $iteration);
+                break;
+            case 'ip':
+            case 'ipv4':
+                $examples = [
+                    "106.198.17.238", "92.249.253.53", "68.8.150.135", "57.37.186.183", "192.89.34.71",
+                    "94.195.220.102", "24.185.102.94", "1.152.115.28", "72.47.37.220", "62.64.250.209",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'ipv6':
+                $examples = [
+                    "bcd2:21a4:3e52:6427:8b21:c58c:74e8:88a7",
+                    "8b44:561e:9514:e750:95a9:57a:aacd:4a37",
+                    "cacc:4f6a:76ba:2d45:cf08:401c:b2d3:e48",
+                    "5547:bd8f:39fe:230c:750c:9e6c:b6d2:6440",
+                    "1fbe:2af5:6f3f:1d1c:eaf4:cae1:8ba1:7e23",
+                    "66e6:1e1e:8bc6:fa4f:279a:32ef:8489:4fac",
+                    "8221:32fe:1ed3:d582:879a:55ca:61c4:7516",
+                    "aa68:41af:f8a6:932f:4e84:bf11:f08a:aead",
+                    "553f:be11:1c25:4da8:88c:d498:83e2:c2b1",
+                    "6cf7:d7d9:11a4:5fd0:7627:a0a:cec4:d5b6",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'float':
+                $example = $iteration * 1.65;
+                break;
+            case 'date':
+                $date = Date::createFromFormat('Y-m-d H:i:s', $dateStr);
+                $date->addSeconds($iteration * 800636);
+                $example = $date->format('Y-m-d');
+                break;
+            case 'date-time':
+            case 'dateTime':
+            case 'datetime':
+                $date = Date::createFromFormat('Y-m-d H:i:s', $dateStr);
+                $date->addSeconds($iteration * 800636);
+                $example = $date->format('Y-m-d H:i:s');
+                break;
+            case 'numeric':
+            case 'integer':
+                $example = intval($iteration * 3);
+                break;
+            case 'boolean':
+                $example = $this->takeFromArray([false, true], $iteration);
+                break;
+            case 'first_name':
+                $examples = [
+                    "Myriam", "Sylvan", "Eldred", "Joana", "Carson", "Madisyn", "Trever", "Scotty", "Oran", "Guadalupe",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'last_name':
+                $examples = [
+                    "Fay", "Cartwright", "Hansen", "Swift", "Crooks", "Ortiz", "Johns", "Howell", "Stehr", "Brown"
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            case 'address':
+                $examples = [
+                    "611 Thompson Way Suite 012\nEast Elza, FL 57666-8738",
+                    "30319 Fay Spurs Apt. 662\nMurphystad, LA 07944",
+                    "779 Lamont Landing\nPort Allenburgh, AL 54941-5287",
+                    "8360 Imogene Turnpike Suite 023\nLake Baby, FL 26431",
+                    "60943 Keira Turnpike\nSteuberport, DE 69137-2357",
+                    "77390 Koby Crescent Suite 624\nWintheiserton, AR 67349",
+                    "117 Rice Ramp Suite 251\nSouth Horace, AK 97749-6015",
+                    "1825 Tiara Path\nNew Maia, SC 74108",
+                    "790 Sallie Rest\nMaggioland, CO 32943",
+                    "2523 Bergnaum Ferry Suite 247\nJoaquinberg, MS 54512",
+                ];
+                $example = $this->takeFromArray($examples, $iteration);
+                break;
+            default:
+                $example = null;
+        }
+        return $example;
+    }
+
+    /**
      * Get example by given type
      * @param  string $type
      * @return array|int|string|null
@@ -63,6 +327,73 @@ trait WithExampleGenerator
     protected function exampleByType($type)
     {
         $type = is_string($type) ? $this->normalizeType($type, true) : null;
+        $key = $type;
+        if (!isset($this->varsSequences[$key])) {
+            $this->varsSequences[$key] = $this->generateExampleByTypeSequence($type, 10);
+        }
+        $example = current($this->varsSequences[$key]);
+        if (next($this->varsSequences[$key]) === false) {
+            reset($this->varsSequences[$key]);
+        }
+        return $example;
+    }
+
+    /**
+     * Get example value by validation rule
+     * @param  string $rule
+     * @return mixed
+     */
+    protected function exampleByRule(string $rule)
+    {
+        $key = '__' . $rule;
+        if (!isset($this->varsSequences[$key])) {
+            $this->varsSequences[$key] = $this->generateExampleByRuleSequence($rule, 10);
+        }
+        $example = current($this->varsSequences[$key]);
+        if (next($this->varsSequences[$key]) === false) {
+            reset($this->varsSequences[$key]);
+        }
+        return $example;
+    }
+
+    /**
+     * Get variable value from cache
+     * @param  string $name
+     * @param  string $type
+     * @return mixed|null
+     * @internal
+     */
+    protected function getVarCache($name, $type)
+    {
+        if (($key = $this->getVarCacheKey($name, $type)) === null) {
+            return null;
+        }
+        return Arr::get($this->varsCache, $key);
+    }
+
+    /**
+     * Set variable value to cache
+     * @param  string $name
+     * @param  string $type
+     * @param  mixed $value
+     * @return mixed|null
+     * @internal
+     */
+    protected function setVarCache($name, $type, $value)
+    {
+        if ($value !== null && ($key = $this->getVarCacheKey($name, $type)) !== null) {
+            Arr::set($this->varsCache, $key, $value);
+        }
+        return $value;
+    }
+
+    /**
+     * @internal
+     * @param  string $type
+     * @return mixed
+     */
+    protected function exampleByTypeInternal($type)
+    {
         switch ($type) {
             case 'int':
             case 'integer':
@@ -73,7 +404,7 @@ trait WithExampleGenerator
                 return $this->faker()->randomFloat(2);
                 break;
             case 'string':
-                return array_random(['string', 'value', 'str value']);
+                return Arr::random(['string', 'value', 'str value']);
                 break;
             case 'bool':
             case 'boolean':
@@ -95,15 +426,15 @@ trait WithExampleGenerator
     }
 
     /**
-     * Get example value by validation rule
+     * @internal
      * @param  string $rule
      * @return mixed
      */
-    protected function exampleByRule(string $rule)
+    protected function exampleByRuleInternal(string $rule)
     {
         switch ($rule) {
             case 'phone':
-                $example = array_random(['+380971234567', '+380441234567', '+15411234567', '+4901511234567']);
+                $example = Arr::random(['+380971234567', '+380441234567', '+15411234567', '+4901511234567']);
                 break;
             case 'url':
                 $example = $this->faker()->url;
@@ -115,20 +446,20 @@ trait WithExampleGenerator
                 $example = $this->faker()->email;
                 break;
             case 'password':
-                $example = str_random(16);
+                $example = Str::random(16);
                 break;
             case 'token':
-                $example = str_random(64);
+                $example = Str::random(64);
                 break;
             case 'service_name':
-                $example = array_random(['fb', 'google', 'twitter']);
+                $example = Arr::random(['fb', 'google', 'twitter']);
                 break;
             case 'domain_name':
                 $example = $this->faker()->domainName;
                 break;
             case 'alpha':
             case 'string':
-                $example = array_random(['string', 'value', 'str value']);
+                $example = Arr::random(['string', 'value', 'str value']);
                 break;
             case 'text':
                 $example = $this->faker()->text(100);
@@ -137,10 +468,10 @@ trait WithExampleGenerator
                 $example = $this->faker()->text(50);
                 break;
             case 'alpha_num':
-                $example = array_random(['string35', 'value90', 'str20value']);
+                $example = Arr::random(['string35', 'value90', 'str20value']);
                 break;
             case 'alpha_dash':
-                $example = array_random(['string_35', 'value-90', 'str_20-value']);
+                $example = Arr::random(['string_35', 'value-90', 'str_20-value']);
                 break;
             case 'ip':
             case 'ipv4':
@@ -183,37 +514,6 @@ trait WithExampleGenerator
     }
 
     /**
-     * Get variable value from cache
-     * @param  string $name
-     * @param  string $type
-     * @return mixed|null
-     * @internal
-     */
-    protected function getVarCache($name, $type)
-    {
-        if (($key = $this->getVarCacheKey($name, $type)) === null) {
-            return null;
-        }
-        return Arr::get($this->varsCache, $key);
-    }
-
-    /**
-     * Set variable value to cache
-     * @param  string $name
-     * @param  string $type
-     * @param  mixed $value
-     * @return mixed|null
-     * @internal
-     */
-    protected function setVarCache($name, $type, $value)
-    {
-        if ($value !== null && ($key = $this->getVarCacheKey($name, $type)) !== null) {
-            Arr::set($this->varsCache, $key, $value);
-        }
-        return $value;
-    }
-
-    /**
      * Create variable cache string key
      * @param  string $name
      * @param  string $type
@@ -233,5 +533,22 @@ trait WithExampleGenerator
             }
         }
         return $name . '|' . $type;
+    }
+
+    /**
+     * Take value from array even if it does not exists by given offset
+     * @param  array $array
+     * @param  int $number
+     * @return mixed
+     */
+    private function takeFromArray($array, $number)
+    {
+        if (empty($array)) {
+            return null;
+        }
+        if (!isset($array[$number])) {
+            $number = $number % count($array);
+        }
+        return $array[$number];
     }
 }
